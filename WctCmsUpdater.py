e6af2b1ba2a5179eefae423c84d96296fbf6aa8c
Ben O'Brien <ben.o'brien@dia.govt.nz>
2016-07-29 12:27:48 +1200

    DB update and rollback added.

--- WctCmsUpdater.py
+++ WctCmsUpdater.py
@@ -3,7 +3,7 @@ import sys, csv
 import pyodbc
 
 args_num = len(sys.argv)
-print("Number of arguments provided: ", args_num)
+print("==============================")
 print('Argument List: ', str(sys.argv))
 
 """ Parse command line arguments """
@@ -18,19 +18,12 @@ else:
 	print("Too many arguments provided")
 	sys.exit()
 
-"""Take a filename as an argument"""
+print("==============================\n")
 
 
 
 
-"""Read the csv file into a list"""
-"""
-with open(input_file, 'r') as f:
-	reader = csv.reader(f)
-	voyager_list = list(reader)
-	
-print(voyager_list)
-"""
+
 
 
 wct_ids = list()
@@ -50,8 +43,10 @@ cursor.execute("SELECT DISTINCT(at_reference) AS ref FROM db_wct.abstract_target
 for row in cursor:
 	wct_ids.append(row[0])
 	
+print("==============================")
+print("Voyager IDs extracted from DB")
+print("==============================")
 print(wct_ids)
-
 
 
 """ Read csv file of Alma-Voyager IDs into list """
@@ -61,17 +56,21 @@ with open(input_file, 'r') as f:
 
 
 """ Cross reference with csv file to build dictionary of IDs to update """
+print("\n")
+print("==============================")
+print("Searching csv file matches...")
+print("==============================")
+
 for row in voyager_alma_list:
 	if row[1] in wct_ids:
-		print("Found Voyager ID in csv: ", row[1])
 		new_wct_ids[row[1]] = row[0]
+		print("Found Voyager ID in csv: ", row[1])
 
-print(new_wct_ids)
 
 		
 """ Once dictionary is built, then prompt user asking if they want to continue with updating db """
 voyager_alma_list = None
-
+print("\n")
 choice = input("Dictonary has been built, do you want to continue updating WCT database? (y/n)").lower()
 if choice != "y":
 	sys.exit()
@@ -84,7 +83,40 @@ if row:
     print(row)
 	
 	
-"""cursor.execute("UPDATE abstract_target SET at_reference =? WHERE at_reference =?")"""
+print("\n")
+print("==============================")
+print("Updating WCT database")
+print("==============================")
 
+for voyager_id, alma_id in new_wct_ids.items():
+	print("Updating {} with {}".format(voyager_id, alma_id))
+	print("UPDATE abstract_target SET at_reference = {} WHERE at_reference = {}".format(alma_id, voyager_id))
+	cursor.execute("UPDATE db_wct.abstract_target SET at_reference = ? WHERE at_reference = ?", str(alma_id), str(voyager_id))
+	cnxn.commit()
+	
+	
+	
+print("\n")
+choice = input("Update complete. Roll back updates? (y/n)").lower()
+if choice != "y":
+	sys.exit()
+	
+for voyager_id, alma_id in new_wct_ids.items():
+	cursor.execute("UPDATE db_wct.abstract_target SET at_reference = ? WHERE at_reference = ?", str(voyager_id), str(alma_id))
+	cnxn.commit()
 
+print("Rollback complete")
+
+	
 cnxn.close()
+
+
+
+
+
+
+
+
+
+
+
